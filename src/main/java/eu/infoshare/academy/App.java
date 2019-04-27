95a7d09df0af72e481db38c0eb7f24fb4ca76aa6
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-04-27 16:19:50 +0200

    Add RestResponse and StateInfo class

---
+++ src/main/java/eu/infoshare/academy/App.java
@@ -0,0 +1,5 @@
+package eu.infoshare.academy;
+
+public class App
+{
+}
---
+++ src/main/java/eu/infoshare/academy/model/RestResponse.java
@@ -0,0 +1,41 @@
+package eu.infoshare.academy.model;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.util.List;
+
+public class RestResponse {
+
+    @JsonProperty("RestResponse")
+    private RestResponse restResponse;
+
+    private List<String> messages;
+    private List<StateInfo> result;
+
+    public RestResponse() {
+    }
+
+    public RestResponse getRestResponse() {
+        return restResponse;
+    }
+
+    public void setRestResponse(RestResponse restResponse) {
+        this.restResponse = restResponse;
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<String> messages) {
+        this.messages = messages;
+    }
+
+    public List<StateInfo> getResult() {
+        return result;
+    }
+
+    public void setResult(List<StateInfo> result) {
+        this.result = result;
+    }
+}
